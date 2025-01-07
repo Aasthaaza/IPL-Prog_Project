9f64053b334529a717faacef03ec02daa091bd89
username <wecp-dummy@gmail.com>
2025-01-07 13:23:06 +0000

    wecp initial commit

---
+++ backend/src/main/java/com/wecp/progressive/exception/TeamAlreadyExistsException.java
@@ -0,0 +1,8 @@
+
+package com.wecp.progressive.exception;
+
+public class TeamAlreadyExistsException extends RuntimeException{
+    public TeamAlreadyExistsException(String msg){
+        super(msg);
+    }
+}
---
+++ backend/src/main/java/com/wecp/progressive/repository/TeamRepository.java
@@ -0,0 +1,13 @@
+package com.wecp.progressive.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.wecp.progressive.entity.Team;
+
+@Repository
+public interface TeamRepository extends JpaRepository<Team, Integer>{
+    Team findByTeamId(int teamId);
+
+    Team findByTeamName(String name);
+}
