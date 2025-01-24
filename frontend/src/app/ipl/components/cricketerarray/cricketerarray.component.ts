b7deecb899064ff160d24318c429ef771f0e7dd2
username <wecp-dummy@gmail.com>
2025-01-24 13:09:15 +0000

    Day 18 Task

--- frontend/src/app/ipl/components/cricketerarray/cricketerarray.component.ts
+++ frontend/src/app/ipl/components/cricketerarray/cricketerarray.component.ts
@@ -1,6 +1,22 @@
+import { OnInit } from "@angular/core";
+import { Cricketer } from "../../types/Cricketer";
 
-export class CricketerArrayComponent  {
+export class CricketerArrayComponent implements OnInit {
+    cricketers: Cricketer[] = [];
+    showCricketers: boolean = true;
 
- 
+    constructor() {}
 
-}
+    ngOnInit(): void {
+        this.cricketers = [
+            new Cricketer(1, 101, 'Virat Kohli', 32, 'Indian', 12, 'Batsman', 12000, 4),
+            new Cricketer(2, 102, 'AB de Villiers', 37, 'South Africa', 15, 'Batsman', 9500, 1),
+            new Cricketer(3, 103, 'Jasprit Bumrah', 28, 'Indian', 8, 'Bowler', 200, 250),
+            new Cricketer(4, 104, 'Ben Stokes', 30, 'ENglish', 10, 'All-Rounder', 4500, 150)
+        ];
+        
+        }
+        toggleCricketers(): void {
+            this.showCricketers = !this.showCricketers;
+        }
+    }
--- frontend/src/app/ipl/components/teamcreate/teamcreate.component.ts
+++ frontend/src/app/ipl/components/teamcreate/teamcreate.component.ts
@@ -1,4 +1,36 @@
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
-export class TeamCreateComponent {
+@Component({
+    selector: 'app-teamcreate',
+    templateUrl: './teamcreate.component.html',
+    styleUrls: ['./teamcreate.component.scss']
+})
+export class TeamCreateComponent implements OnInit {
+    teamForm!: FormGroup;
+    successMessage: string | null = null;
+    errorMessage: string | null = null;
+
+    constructor(private formBuilder: FormBuilder) {}
+
+    ngOnInit(): void {
+        this.teamForm = this.formBuilder.group({
+            teamId: [null, [Validators.required, Validators.min(1)]],
+            teamName: ['', [Validators.required, Validators.minLength(2)]],
+            location: ['', [Validators.required]],
+            ownerName: ['', [Validators.required, Validators.minLength(2)]],
+            establishmentYear: [null, [Validators.required]]
+        });
+    }
+
+    onSubmit(): void {
+        this.teamForm.reset({
+            teamId: null,
+            teamName: '',
+            location: '',
+            ownerName: '',
+            establishmentYear: new Date().getFullYear()
+        });
+    }
  
 }
